e15cb8e6b0c1afda7cb72a354d29049501947df0
nikhilkumar314 <54560972+nikhilkumar314@users.noreply.github.com>
2019-09-10 11:17:27 -0400

    Add files via upload

---
+++ xlspop.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[10]:
+
+
+Location = "PST01.xls"
+
+
+# In[11]:
+
+
+df = pd.read_excel(Location)
+
+
+# In[12]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
