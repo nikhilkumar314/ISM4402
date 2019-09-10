e042ed2e2d354932c264812198bc2ce59c8ee49b
nikhilkumar314 <54560972+nikhilkumar314@users.noreply.github.com>
2019-09-10 11:11:44 -0400

    Add files via upload

---
+++ FL pop.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+Location = "all_040_in_12.P1.csv"
+
+
+# In[6]:
+
+
+df = pd.read_csv(Location)
+
+
+# In[7]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
